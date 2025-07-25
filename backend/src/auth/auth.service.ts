8cf7831583341bb92d77fbac342a53a7130a1964
giakhang94 <ngk.khang94@gmail.com>
2025-07-25 22:20:42 +0700

    feat: #3 - local-strategy

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { comparePw } from 'src/utils/hassPassword';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+  async validateUser(email: string, password: string) {
+    if (!email) console.log('can not work correctly without email');
+    const user = await this.usersService.findUserByEmail(email);
+    if (await comparePw(password, user.password)) {
+      return user;
+    } else {
+      throw new BadRequestException('Email and password not match');
+    }
+  }
+}
