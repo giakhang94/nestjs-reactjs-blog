41d3ef6f581385d10e991aa43f95a412efcb4c3e
giakhang94 <ngk.khang94@gmail.com>
2025-07-26 11:25:12 +0700

    feat: #3 - login and attach token to cookie

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,28 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategy/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.getOrThrow('JWT_SECRET'),
+          global: true,
+          signOptions: {
+            expiresIn:
+              configService.getOrThrow('JWT_EXP') +
+              configService.getOrThrow('JWT_TIME_UNIT'),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,19 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
 import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { comparePw } from 'src/utils/hassPassword';
+import { attachToken } from './helpers/attach-token';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwt: JwtService,
+    private configService: ConfigService,
+  ) {}
   async validateUser(email: string, password: string) {
     if (!email) console.log('can not work correctly without email');
     const user = await this.usersService.findUserByEmail(email);
@@ -18,6 +25,9 @@ export class AuthService {
   }
 
   async loginUser(user: User, response: Response) {
+    const payload = { userId: user.id };
+    const token = this.jwt.sign(payload);
+    attachToken(this.configService.getOrThrow('JWT_EXP'), token, response);
     return user;
   }
 }
