11cf1941abf18f11028481a8db5e3c283e7abccb
giakhang94 <ngk.khang94@gmail.com>
2025-07-26 12:50:37 +0700

    feat: #3 - jwt strategy - cookie parser

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  validate(email: string, password: string) {}
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req.cookies!['authentication'];
+          // console.log(req.headers.cookie?.split('=')[1]);
+          // return req.headers.Cookie!['authentication'];
+          // return req.headers.cookie?.split('=')[1] as string;
+        },
+      ]),
+      secretOrKey: configService.getOrThrow('JWT_SECRET'),
+    });
+  }
+  async validate(payload: any) {
+    //mặc định tự nhận
+    return payload;
+  }
 }
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { GetCurrentUser } from 'src/auth/decorators/GetCurrentUser.decorator';
+import { User } from './user.entity';
+import { JwtGuard } from 'src/auth/guards/Jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+  @Get('me')
+  @UseGuards(JwtGuard)
+  async getMe(@GetCurrentUser() user: Partial<User>) {
+    return user;
+  }
 }
