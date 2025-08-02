f4c9f578b08d6da21077a6e8344d7fb354a49dc0
giakhang94 <ngk.khang94@gmail.com>
2025-08-02 21:06:46 +0700

    feat: #27 -Tag - get all tags fulltext search

---
+++ backend/prisma/migrations/20250802140428_add_tag_fulltext/migration.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE FULLTEXT INDEX `Tag_tag_idx` ON `Tag`(`tag`);
--- backend/src/tag/tag.module.ts
+++ backend/src/tag/tag.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TagService } from './tag.service';
+import { TagController } from './tag.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [TagService]
+  providers: [TagService],
+  imports: [PrismaModule],
+  controllers: [TagController],
 })
 export class TagModule {}
