d30925882454f848c522744cf2022d8c8968a0c7
giakhang94 <ngk.khang94@gmail.com>
2025-08-14 21:06:51 +0700

    test: #35 - refresh token, logout user, login user

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,7 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 jest.mock('src/utils/hassPassword', () => {
@@ -10,22 +14,38 @@ jest.mock('src/utils/hassPassword', () => {
   };
 });
 import * as passwordUtils from 'src/utils/hassPassword';
+import { response, Response } from 'express';
+import { attachToken } from './helpers/attach-token';
+
 describe('AuthService', () => {
   let service: AuthService;
   let userService: {
     findUserByEmail: any;
   };
+  let jwtService: {
+    sign: any;
+    verify: any;
+    verifyAsync: any;
+  };
+  let configService: jest.Mocked<ConfigService>;
+  let resMock: jest.Mocked<Response>;
 
   beforeEach(async () => {
     userService = {
       findUserByEmail: jest.fn(),
     };
 
+    jwtService = {
+      sign: jest.fn(),
+      verify: jest.fn(),
+      verifyAsync: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         AuthService,
         ConfigService,
-        JwtService,
+        { provide: JwtService, useValue: jwtService },
         { provide: UsersService, useValue: userService },
       ],
     }).compile();
@@ -67,6 +87,112 @@ describe('AuthService', () => {
       await expect(
         service.validateUser('input email', 'input password'),
       ).rejects.toThrow(BadRequestException);
+    });
+  });
+  describe('loginUser, logoutU ser', () => {
+    const mock_token = 'mock_token as a string';
+    beforeEach(async () => {
+      configService = {
+        getOrThrow: jest.fn((key: string) => {
+          const fakeConfig = {
+            JWT_REFRESH_EXP: 1,
+            JWT_TIME_UNIT: 'h',
+            JWT_REFRESH_SECRET: 'refresh_secret',
+            JWT_EXP: 1,
+          } as any;
+          return fakeConfig[key];
+        }),
+      } as any;
+      jwtService.sign.mockReturnValue(mock_token);
+      resMock = { cookie: jest.fn() } as any;
+
+      jest.clearAllMocks();
+    });
+
+    it('should sign tokens and set cookies', async () => {
+      const fakeUser = { id: 123, role: 'admin' } as any;
+      const result = await service.loginUser(fakeUser, resMock);
+
+      expect(jwtService.sign).toHaveBeenCalledTimes(2);
+      expect(resMock.cookie).toHaveBeenCalledWith(
+        'refresh',
+        mock_token,
+        expect.any(Object),
+      );
+      expect(resMock.cookie).toHaveBeenCalledWith(
+        'authentication',
+        mock_token,
+        expect.any(Object),
+        //expect.any(Object) => check những phần còn lại là Object là được
+        // để k bỏ sót đối số, nhưng đối số đó k qtrong nên k  cần ghi rõ, thfi dùng expect.any
+      );
+      expect(result).toEqual(fakeUser);
+    });
+
+    it('should logout user by attaching empty value tokens to cookie', async () => {
+      let empty_token = '';
+      // let expire_time = 0;
+      const result = await service.logoutUser(resMock);
+
+      expect(result).toEqual({ message: 'logout successfully' });
+      expect(resMock.cookie).toHaveBeenCalledTimes(2);
+      expect(resMock.cookie).toHaveBeenCalledWith(
+        'authentication',
+        empty_token,
+        expect.any(Object),
+      );
+      expect(resMock.cookie).toHaveBeenCalledWith(
+        'refresh',
+        empty_token,
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe.only('refresh token', () => {
+    beforeEach(() => {
+      resMock = { cookie: jest.fn() } as any;
+
+      jest.clearAllMocks();
+    });
+    it('should throw new UnauthorizedException if token is falsy', async () => {
+      await expect(service.refreshToken('', resMock)).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
+
+    it('should throw new UnauthorizedException when token is expires', async () => {
+      jwtService.verifyAsync.mockResolvedValue({
+        userId: 1,
+        role: 'admin',
+        exp: 0,
+      });
+      await expect(service.refreshToken('mock_token', resMock)).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
+
+    it('should call jwt.sign and attachToken correctly', async () => {
+      jwtService.verifyAsync.mockResolvedValue({
+        userId: 1,
+        role: 'admin',
+        exp: Date.now() + 1000 * 60 * 60 * 24,
+      });
+      jwtService.sign.mockReturnValue('new access token');
+      const result = await service.refreshToken('mock_token', resMock);
+      expect(jwtService.verifyAsync).toHaveBeenCalledWith(
+        'mock_token',
+        expect.any(Object),
+      );
+
+      expect(jwtService.sign).toHaveBeenCalledTimes(1);
+      expect(resMock.cookie).toHaveBeenCalledWith(
+        'authentication',
+        'new access token',
+        expect.any(Object),
+      );
+
+      expect(result).toEqual({ message: 'access-token refreshed' });
     });
   });
 });
