857feb58389754e3465e6f8620bd1fed05f1d358
giakhang94 <ngk.khang94@gmail.com>
2025-08-16 11:36:18 +0700

    test: #39 - create post rollback

--- backend/src/post/post.service.spec.ts
+++ backend/src/post/post.service.spec.ts
@@ -11,10 +11,11 @@ jest.mock('./helpers/create-unique-slug', () => {
 import * as createUniqueSlug from './helpers/create-unique-slug';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('PostService', () => {
   let service: PostService;
-  let cloudinary: CloudinaryService;
+  let cloudinary: { uploadFile: any; deleteFile: any };
   let prisma: { $transaction: ({ tx }: any) => any };
   beforeEach(async () => {
     cloudinary = {
@@ -97,6 +98,31 @@ describe('PostService', () => {
           slug: 'unique slug',
         }),
       });
+      expect(post).toBeDefined();
+      expect(cloudinary.uploadFile).toHaveBeenCalledTimes(1);
+      expect(cloudinary.uploadFile).toHaveBeenCalledWith(mockFile);
+    });
+
+    it('should rollback and delete uploaded file on cloudinary when something fails', async () => {
+      (prisma.$transaction as jest.Mock).mockImplementation(async (cb) => {
+        return cb({
+          post: {
+            create: jest.fn().mockRejectedValue(new BadRequestException()),
+          },
+        });
+      });
+      cloudinary.uploadFile.mockResolvedValue({
+        public_id: '1235',
+      });
+      // (cloudinary.deleteFile as jest.Mock).mockResolvedValue('1235');
+      await expect(
+        service.createPost(body, mockUserPayload, mockFile),
+      ).rejects.toThrow(BadRequestException);
+
+      // expect(cloudinary.deleteFile).toHaveBeenCalledTimes(1);
+      // expect(cloudinary.deleteFile).toHaveBeenCalledWith('1235');
+      expect(cloudinary.deleteFile).toHaveBeenCalledWith('1235');
+      expect(cloudinary.deleteFile).toHaveBeenCalledTimes(1);
     });
   });
 });
